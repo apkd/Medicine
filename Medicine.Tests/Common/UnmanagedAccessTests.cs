bcd06605824dc0bafe09574c2520d5937993006b
apk <apkd@users.noreply.github.com>
2026-01-25 13:40:25 +0100

    Add more UnmanagedAccessTests

--- Medicine.Tests/Common/UnmanagedAccessTests.cs
+++ Medicine.Tests/Common/UnmanagedAccessTests.cs
@@ -4,6 +4,10 @@ using NUnit.Framework;
 using UnityEngine;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using Medicine.Internal;
+using static System.Reflection.BindingFlags;
 
 [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
 [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
@@ -12,6 +16,10 @@ using System.Diagnostics.CodeAnalysis;
 [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
 public partial class UnmanagedAccessTests
 {
+    static bool HasIsReadOnlyAttribute(ParameterInfo parameterInfo)
+        => parameterInfo.GetRequiredCustomModifiers().Any(modifier => modifier.Name == "IsReadOnlyAttribute") ||
+           parameterInfo.GetCustomAttributes(inherit: false).Any(attribute => attribute.GetType().Name == "IsReadOnlyAttribute");
+
     [UnmanagedAccess]
     public partial class BasicFields
     {
@@ -177,6 +185,282 @@ public partial class UnmanagedAccessTests
         Assert.That(BasicFields.Unmanaged.ClassLayout.privateIntField, Is.GreaterThan(0));
         Assert.That(BasicFields.Unmanaged.ClassLayout.IntField, Is.Not.EqualTo(BasicFields.Unmanaged.ClassLayout.FloatField));
         Assert.That(BasicFields.Unmanaged.ClassLayout.IntField, Is.Not.EqualTo(BasicFields.Unmanaged.ClassLayout.privateIntField));
+    }
+
+    [Test]
+    public void UnmanagedAccess_BasicFields_ReadOnly()
+    {
+        var obj = new BasicFields();
+        UnmanagedRef<BasicFields> unmanagedRef = obj;
+        var access = unmanagedRef.AccessRO();
+
+        Assert.That(access.IntField, Is.EqualTo(10));
+        Assert.That(access.FloatField, Is.EqualTo(20.0f));
+        Assert.That(access.BoolField, Is.True);
+        Assert.That(access.InternalDoubleField, Is.EqualTo(30.0));
+        Assert.That(access.privateIntField, Is.EqualTo(40));
+    }
+
+    [UnmanagedAccess]
+    public partial class LayoutSwapFields
+    {
+        public int First = 1;
+        public int Second = 2;
+    }
+
+    [Test]
+    public void UnmanagedAccess_ExplicitLayoutRef_SwapsFields()
+    {
+        var obj = new LayoutSwapFields { First = 10, Second = 20 };
+        UnmanagedRef<LayoutSwapFields> unmanagedRef = obj;
+        var swappedLayout = new LayoutSwapFields.Unmanaged.Layout
+        {
+            First = LayoutSwapFields.Unmanaged.ClassLayout.Second,
+            Second = LayoutSwapFields.Unmanaged.ClassLayout.First,
+        };
+
+        var accessRO = unmanagedRef.AccessRO(ref swappedLayout);
+        Assert.That(accessRO.First, Is.EqualTo(20));
+        Assert.That(accessRO.Second, Is.EqualTo(10));
+
+        var accessRW = unmanagedRef.AccessRW(ref swappedLayout);
+        accessRW.First = 100;
+        accessRW.Second = 200;
+
+        Assert.That(obj.First, Is.EqualTo(200));
+        Assert.That(obj.Second, Is.EqualTo(100));
+    }
+
+    [UnmanagedAccess]
+    public partial class ReadOnlyFields
+    {
+        public readonly int ReadOnlyValue = 5;
+        public int WritableValue = 7;
+    }
+
+    [Test]
+    public void UnmanagedAccess_AccessRO_ReturnsReadOnlyRefs()
+    {
+#if !ENABLE_IL2CPP
+        var accessROProperty
+            = typeof(ReadOnlyFields.Unmanaged.AccessRO)
+                .GetProperty(nameof(ReadOnlyFields.WritableValue));
+
+        Assert.That(accessROProperty, Is.Not.Null);
+        Assert.That(HasIsReadOnlyAttribute(accessROProperty!.GetMethod!.ReturnParameter), Is.True);
+
+        var accessRWReadOnlyProperty
+            = typeof(ReadOnlyFields.Unmanaged.AccessRW)
+                .GetProperty(nameof(ReadOnlyFields.ReadOnlyValue));
+
+        Assert.That(accessRWReadOnlyProperty, Is.Not.Null);
+        Assert.That(HasIsReadOnlyAttribute(accessRWReadOnlyProperty!.GetMethod!.ReturnParameter), Is.True);
+
+        var accessRWWritableProperty
+            = typeof(ReadOnlyFields.Unmanaged.AccessRW)
+                .GetProperty(nameof(ReadOnlyFields.WritableValue));
+
+        Assert.That(accessRWWritableProperty, Is.Not.Null);
+        Assert.That(HasIsReadOnlyAttribute(accessRWWritableProperty!.GetMethod!.ReturnParameter), Is.False);
+#else
+        Assert.Inconclusive("Fails to compile under IL2CPP for some reason");
+#endif
+    }
+
+    [UnmanagedAccess(includePrivate: false, includePublic: true)]
+    public partial class FilterPublicOnlyFields
+    {
+        public int PublicField = 1;
+        int privateField = 2;
+    }
+
+    [UnmanagedAccess(includePrivate: true, includePublic: false)]
+    public partial class FilterPrivateOnlyFields
+    {
+        public int PublicField = 1;
+        int privateField = 2;
+    }
+
+    [UnmanagedAccess("IncludedOne", "IncludedTwo")]
+    public partial class FilterMemberNamesFields
+    {
+        public int IncludedOne = 10;
+        public int IncludedTwo = 20;
+        public int Excluded = 30;
+    }
+
+    [UnmanagedAccess(safetyChecks: false)]
+    public partial class NoSafetyChecksFields
+    {
+        public int Value = 1;
+    }
+
+    [Test]
+    public void UnmanagedAccess_Filters_PublicOnly()
+    {
+        var accessType = typeof(FilterPublicOnlyFields.Unmanaged.AccessRW);
+        Assert.That(accessType.GetProperty("PublicField"), Is.Not.Null);
+        Assert.That(accessType.GetProperty("privateField"), Is.Null);
+    }
+
+    [Test]
+    public void UnmanagedAccess_Filters_PrivateOnly()
+    {
+        var accessType = typeof(FilterPrivateOnlyFields.Unmanaged.AccessRW);
+        Assert.That(accessType.GetProperty("PublicField"), Is.Null);
+        Assert.That(accessType.GetProperty("privateField"), Is.Not.Null);
+    }
+
+    [Test]
+    public void UnmanagedAccess_Filters_MemberNames()
+    {
+        var accessType = typeof(FilterMemberNamesFields.Unmanaged.AccessRW);
+        Assert.That(accessType.GetProperty(nameof(FilterMemberNamesFields.IncludedOne)), Is.Not.Null);
+        Assert.That(accessType.GetProperty(nameof(FilterMemberNamesFields.IncludedTwo)), Is.Not.Null);
+        Assert.That(accessType.GetProperty(nameof(FilterMemberNamesFields.Excluded)), Is.Null);
+    }
+
+    [Test]
+    public void UnmanagedAccess_SafetyChecksDisabled_SkipsCheckMethods()
+    {
+        var accessRWType = typeof(NoSafetyChecksFields.Unmanaged.AccessRW);
+        var accessROType = typeof(NoSafetyChecksFields.Unmanaged.AccessRO);
+
+        Assert.That(accessRWType.GetMethod("CheckNull", Instance | NonPublic), Is.Null);
+        Assert.That(accessROType.GetMethod("CheckNull", Instance | NonPublic), Is.Null);
+    }
+
+    [UnmanagedAccess]
+    public partial class AutoPropertyFields
+    {
+        public int AutoInt { get; set; } = 7;
+        public BasicFields AutoRef { get; set; } = new() { IntField = 55 };
+        public int ManualField = 3;
+    }
+
+    [Test]
+    public void UnmanagedAccess_AutoProperties_ReadWrite()
+    {
+        var other = new BasicFields { IntField = 9 };
+        var obj = new AutoPropertyFields { AutoInt = 5, AutoRef = other };
+        UnmanagedRef<AutoPropertyFields> unmanagedRef = obj;
+        var access = unmanagedRef.AccessRW();
+
+        Assert.That(access.AutoInt, Is.EqualTo(5));
+        Assert.That(access.AutoRef.AccessRO().IntField, Is.EqualTo(9));
+
+        access.AutoInt = 42;
+        var newOther = new BasicFields { IntField = 99 };
+        access.AutoRef = newOther;
+
+        Assert.That(obj.AutoInt, Is.EqualTo(42));
+        Assert.That(obj.AutoRef, Is.SameAs(newOther));
+    }
+
+    [Test]
+    public void UnmanagedAccess_AutoProperties_DeclaredAtMetadata()
+    {
+        var autoIntProperty
+            = typeof(AutoPropertyFields.Unmanaged.AccessRW)
+                .GetProperty(nameof(AutoPropertyFields.AutoInt));
+
+        Assert.That(autoIntProperty, Is.Not.Null);
+        Assert.That(autoIntProperty!.GetCustomAttributes(typeof(DeclaredAtAttribute), false), Is.Not.Empty);
+
+        var manualFieldProperty
+            = typeof(AutoPropertyFields.Unmanaged.AccessRW)
+                .GetProperty(nameof(AutoPropertyFields.ManualField));
+
+        Assert.That(manualFieldProperty, Is.Not.Null);
+        Assert.That(manualFieldProperty!.GetCustomAttributes(typeof(DeclaredAtAttribute), false), Is.Empty);
+
+        var autoIntReadOnlyProperty
+            = typeof(AutoPropertyFields.Unmanaged.AccessRO)
+                .GetProperty(nameof(AutoPropertyFields.AutoInt));
+
+        Assert.That(autoIntReadOnlyProperty, Is.Not.Null);
+        Assert.That(autoIntReadOnlyProperty!.GetCustomAttributes(typeof(DeclaredAtAttribute), false), Is.Not.Empty);
+    }
+
+    [Track]
+    [UnmanagedAccess]
+    public partial class TrackedAccessComponent : MonoBehaviour
+    {
+        public int Value = 1;
+    }
+
+    [Test]
+    public void UnmanagedAccess_TrackedAccessArray_Enumerators()
+    {
+        TestUtility.DestroyAllGameObjects();
+
+        try
+        {
+            var go1 = new GameObject("TrackedAccess1", typeof(TrackedAccessComponent));
+            var go2 = new GameObject("TrackedAccess2", typeof(TrackedAccessComponent));
+            var c1 = go1.GetComponent<TrackedAccessComponent>();
+            var c2 = go2.GetComponent<TrackedAccessComponent>();
+            c1.Value = 1;
+            c2.Value = 2;
+
+            Assert.That(TrackedAccessComponent.Instances.Count, Is.EqualTo(2));
+
+            var accessArray = TrackedAccessComponent.Unmanaged.Instances;
+            Assert.That(accessArray.Length, Is.EqualTo(2));
+
+            int sum = 0;
+            foreach (var access in accessArray)
+            {
+                sum += access.Value;
+                access.Value += 10;
+            }
+
+            Assert.That(sum, Is.EqualTo(3));
+            Assert.That(new[] { c1.Value, c2.Value }, Is.EquivalentTo(new[] { 11, 12 }));
+
+            var readOnly = accessArray.AsReadOnly();
+            Assert.That(readOnly.Length, Is.EqualTo(accessArray.Length));
+
+            int readOnlySum = 0;
+            foreach (var access in readOnly)
+                readOnlySum += access.Value;
+
+            Assert.That(readOnlySum, Is.EqualTo(23));
+            Assert.That(new[] { accessArray[0].Value, accessArray[1].Value }, Is.EquivalentTo(new[] { 11, 12 }));
+            Assert.That(new[] { readOnly[0].Value, readOnly[1].Value }, Is.EquivalentTo(new[] { 11, 12 }));
+        }
+        finally
+        {
+            TestUtility.DestroyAllGameObjects();
+        }
+    }
+
+    [Test]
+    public void UnmanagedRef_Read_UsesLayoutOffsets()
+    {
+        var obj = new BasicFields();
+        UnmanagedRef<BasicFields> unmanagedRef = obj;
+        ref var layout = ref BasicFields.Unmanaged.ClassLayout;
+
+        Assert.That(unmanagedRef.Read<int>(layout.IntField), Is.EqualTo(10));
+        Assert.That(unmanagedRef.Read<float>(layout.FloatField), Is.EqualTo(20.0f));
+
+        unmanagedRef.Read<int>(layout.IntField) = 77;
+        unmanagedRef.Read<double>(layout.InternalDoubleField) = 123.0;
+
+        Assert.That(obj.IntField, Is.EqualTo(77));
+        Assert.That(obj.InternalDoubleField, Is.EqualTo(123.0));
+    }
+
+    [Test]
+    public void UnmanagedRef_Equals_UsesPointerValue()
+    {
+        var obj = new BasicFields();
+        UnmanagedRef<BasicFields> unmanagedRefA = obj;
+        UnmanagedRef<BasicFields> unmanagedRefB = obj;
+
+        Assert.That(unmanagedRefA.Equals(unmanagedRefB), Is.True);
+        Assert.That(unmanagedRefA.Equals(new(IntPtr.Zero)), Is.False);
     }
 }
 #endif
