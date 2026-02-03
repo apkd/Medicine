471428724b5e8723b158f302638b1d81a7ca8888
apk <apkd@users.noreply.github.com>
2026-02-04 00:09:41 +0100

    Update extensions

--- Medicine.Runtime/Extensions/CollectionExtensions.cs
+++ Medicine.Runtime/Extensions/CollectionExtensions.cs
@@ -4,16 +4,19 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using static System.Runtime.CompilerServices.MethodImplOptions;
-using static Medicine.Internal.Utility;
 
 namespace Medicine
 {
     public static partial class MedicineExtensions
     {
+        /// <summary> Returns the provided list or an empty list if the input is null. </summary>
+        /// <returns>A non-null list. If the input is null, an empty list will be returned.</returns>
         [MethodImpl(AggressiveInlining)]
         public static List<T> OrEmpty<T>(this List<T>? list)
             => list ?? PooledList.Empty<T>();
 
+        /// <summary> Returns the provided array or an empty array if the input is null. </summary>
+        /// <returns>A non-null array. If the input is null, an empty array will be returned.</returns>
         [MethodImpl(AggressiveInlining)]
         public static T[] OrEmpty<T>(this T[]? list)
             => list ?? Array.Empty<T>();
--- Medicine.Runtime/Extensions/UnityExtensions.cs
+++ Medicine.Runtime/Extensions/UnityExtensions.cs
@@ -1,7 +1,5 @@
 #if MEDICINE_EXTENSIONS_LIB
 #nullable enable
-using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using static System.Runtime.CompilerServices.MethodImplOptions;
 using static Medicine.Internal.Utility;
@@ -10,12 +8,18 @@ namespace Medicine
 {
     public static partial class MedicineExtensions
     {
+        /// <summary>
+        /// Returns true when the Unity object is not null and not destroyed.
+        /// </summary>
         [MethodImpl(AggressiveInlining)]
-        public static bool IsNotNull(this UnityEngine.Object? obj)
+        public static bool IsValid(this UnityEngine.Object? obj)
             => IsNativeObjectAlive(obj);
 
+        /// <summary>
+        /// Returns true when the Unity object is null or a reference to a destroyed object.
+        /// </summary>
         [MethodImpl(AggressiveInlining)]
-        public static bool IsNull(this UnityEngine.Object? obj)
+        public static bool IsInvalid(this UnityEngine.Object? obj)
             => IsNativeObjectDead(obj);
     }
 }
